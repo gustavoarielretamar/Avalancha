960ada43d1f5ccec65505b06fa09ba9e8675ceed
Gustavo Ariel Retamar <121575510+gustavoarielretamar@users.noreply.github.com>
2023-03-15 21:13:36 -0300

    Update Exactas_Programa 04 (avalancha).py

--- Exactas_Programa 04 (avalancha).py
+++ Exactas_Programa 04 (avalancha).py
@@ -1,59 +1,64 @@
 # %% LIBRERIAS:
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import random
+import imageio
+import os
+
+
 # %% TABLERO EN 0:
-def crear_tablero(n, m, debug=False):
-    t = np.repeat(" ", (n+2)*(m+2)).reshape((n+2), (m+2))
+def crear_tablero(n, debug=False):
+    t = np.repeat(0, (n + 2) * (n + 2)).reshape((n + 2), (n + 2))
     if debug:
         print(t)
     return t
-# %% BORDES:
+# %% BORDES EN -1:
 def bordes(t, debug=False):
-    filas = t.shape[0]
-    columnas = t.shape[1]
-    for i in range(0, columnas):
-        t[(0, i)] = "M"
-        t[(filas - 1, i)] = "M"
+    for i in range(0, len(t)):
+        t[(0, i)] = -1
+        t[(len(t) - 1, i)] = -1
         i = i + 1
-    for i in range(0, filas):
-        t[(i, 0)] = "M"
-        t[(i, columnas - 1)] = "M"
+    for i in range(0, len(t)):
+        t[(i, 0)] = -1
+        t[(i, len(t) - 1)] = -1
         i = i + 1
     if debug:
         print(t)
     return t
-# %% AGREGO ANIMALES:
-def agregar_animales(t, debug=False):
-    filas = [1, 2, 2, 3, 1]
-    columnas = [3, 1, 3, 1, 1]
-    animal = ["A", "A", "A", "A", "L"]
-    for i in range(len(animal)):
-        t[(filas[i], columnas[i])] = animal[i]
+# %% 4. TABLERO BASE CON 0 en el medio y bordes -1
+def tablero_base(n, debug=False):
+    t = crear_tablero(n)
+    t = bordes(t)
     if debug:
         print(t)
     return t
-# %% VECINOS DE:
+# %% IDENTIFICA LOS BORDES
+def es_borde(t, coord, debug=False):
+    borde = False
+    if t[coord] == -1:
+        borde = True
+    if debug:
+        print(borde)
+    return borde
+# %% 7 CAIDA DE LOS COPOS
+def tirar_copo(t, coord, debug=False):
+    t[coord] = t[coord] + 1
+    if debug:
+        print(t)
+    return t
+# %% 8, 9 y 10 COORDENADA DE VECINOS
 def vecinos_de(t, coord, debug=False):
     vecinos = []
-    f = coord[0]
-    c = coord[1]
-    v1 = (f-1, c-1)
-    c1 = t[(f-1, c-1)] != "M"
-    v2 = (f-1, c)
-    c2 = t[(f-1, c)] != "M"
-    v3 = (f-1, c+1)
-    c3 = t[(f-1, c+1)] != "M"
-    v4 = (f, c+1)
-    c4 = t[(f, c+1)] != "M"
-    v5 = (f+1, c+1)
-    c5 = t[(f+1, c+1)] != "M"
-    v6 = (f+1, c)
-    c6 = t[(f+1, c)] != "M"
-    v7 = (f+1, c-1)
-    c7 = t[(f+1, c-1)] != "M"
-    v8 = (f, c-1)
-    c8 = t[(f, c-1)] != "M"
+    a = coord[0]
+    b = coord[1]
+    v1 = ((a + 1), b)
+    c1 = t[((a + 1), b)] != -1
+    v2 = ((a - 1), b)
+    c2 = t[((a - 1), b)] != -1
+    v3 = (a, (b + 1))
+    c3 = t[(a, (b + 1))] != -1
+    v4 = (a, (b - 1))
+    c4 = t[(a, (b - 1))] != -1
     if c1:
         vecinos.append(v1)
     if c2:
@@ -62,183 +67,95 @@ def vecinos_de(t, coord, debug=False):
         vecinos.append(v3)
     if c4:
         vecinos.append(v4)
-    if c5:
-        vecinos.append(v5)
-    if c6:
-        vecinos.append(v6)
-    if c7:
-        vecinos.append(v7)
-    if c8:
-        vecinos.append(v8)
     if debug:
         print(vecinos)
     return vecinos
-# %% BUSCAR ADYACENTES:
-def buscar_adyacente(t, coord, objetivo, debug=False):
-    adyacentes = vecinos_de(t, coord)
-    respuesta = []
-    j = 0
-    for i in range(len(adyacentes)):
-        if t[adyacentes[i]] == objetivo and j == 0:
-            respuesta.append(adyacentes[i])
-            j += 1
-    if debug:
-        print(respuesta)
-    return respuesta
-# %% ALIMENTARSE:
-def alimentar(t, coord, debug=False):
-    l = 0
-    if t[coord] == "L" and l == 0:
-        comer = buscar_adyacente(t, coord, "A")
-        if comer:
-            t[coord] = " "
-            t[comer[0]] = "L"
-        l += 1
-    else:
-        l += 1
-    if debug:
-        print(t)
-    return t
-# %% REPRODUCIRSE:
-def reproducir(t, coord, debug=False):
-    l = 0
-    if t[coord] == "A" and l == 0:
-        pareja = buscar_adyacente(t, coord, "A")
-        if pareja:
-            decendencia = buscar_adyacente(t, coord, " ")
-            if decendencia:
-                t[decendencia[0]] = "A"
-        l += 1
-    elif t[coord] == "L" and l == 0:
-        pareja = buscar_adyacente(t, coord, "L")
-        if pareja:
-            decendencia = buscar_adyacente(t, coord, " ")
-            if decendencia:
-                t[decendencia[0]] = "L"
-        l += 1
-    else:
-        l += 1
-    if debug:
-        print(t)
-    return t
-# %% MOVERSE:
-
-
-def mover(t, coord, debug=False):
-    l = 0
-    if t[coord] == "A" and l == 0:
-        comer = buscar_adyacente(t, coord, " ")
-        if comer:
-            t[coord] = " "
-            t[comer[0]] = "A"
-        l += 1
-    elif t[coord] == "L" and l == 0:
-        comer = buscar_adyacente(t, coord, " ")
-        if comer:
-            t[coord] = " "
-            t[comer[0]] = "L"
-        l += 1
-    else:
-        l += 1
-    if debug:
-        print(t)
-    return t
-
-# %% FASE ALIMENTARSE:
-def fase_alimentar(t, debug=False):
-    cantidad_filas = t.shape[0]
-    cantidad_columnas = t.shape[1]
-    for i in range(1, cantidad_filas - 1):
-        for j in range(1, cantidad_columnas - 1):
-            alimentar(t, (j, i))
-    if debug:
-        print(t)
-    return t
-# %% FASE REPRODUCIRSE:
-def fase_reproducir(t, debug=False):
-    cantidad_filas = t.shape[0]
-    cantidad_columnas = t.shape[1]
-    for i in range(1, cantidad_filas - 1):
-        for j in range(1, cantidad_columnas - 1):
-            reproducir(t, (i, j))
-    if debug:
-        print(t)
-    return t
-# %% FASE DE MOVERSE:
-def fase_mover(t, debug=False):
-    cantidad_filas = t.shape[0]
-    cantidad_columnas = t.shape[1]
-    for i in range(1, cantidad_filas - 1):
-        for j in range(1, cantidad_columnas - 1):
-            mover(t, (i, j))
-    if debug:
-        print(t)
-    return t
-# %% EVOLUCIONAR:
-def evolucionar(t, debug = False):
-    fase_alimentar(t)
-    fase_reproducir(t)
-    fase_mover(t)
-    if debug:
-        print(t)
-    return t
-# %% EVOLUCIONAR EN EL TIEMPO:
-def evolucionar_en_el_tiempo(t, k, debug=False):
+# %% 11. DESBORDAR
+def desbrodar_pocision(t, coord, debug=False):
+    vecinos = vecinos_de(t, coord)
     i = 0
-    while i < k:
-        t = evolucionar(t)
-        i += 1
-    if debug:
-        print(t)
-# %% MEZCLAR:
-def mezclar_celdas(tablero, debug = False):
-    celdas = []
-    for i in range (1 , len(tablero) - 1) :
-        for j in range (1 , len(tablero) - 1) :
-            celdas.append((i, j))
-    random.shuffle(celdas)
-    if debug:
-        print(celdas)
-    return celdas
-# %% ANIMALES RANDOM:
-def animales_random(t, debug=False):
-    celdas = mezclar_celdas(t)
-    filas = []
-    columnas = []
-    animal = []
-    for i in range(0, len(celdas)):
-        filas.append(celdas[i][0])
-        columnas.append(celdas[i][1])
-    for i in range(0, n_antilopes):
-        animal.append("A")
-    for i in range(0, n_leones):
-        animal.append("L")
-    for i in range(len(animal)):
-        t[(filas[i], columnas[i])] = animal[i]
+    while t[coord] >= 4:
+        t[coord] = t[coord] - 4
+        while i < len(vecinos):
+            tirar_copo(t, vecinos[i])
+            i = i + 1
+        if debug:
+            print(t)
+    return t
+# %% DESBORDAR EL TABLERO:
+def desbordar_valle(t, debug=False):
+    cantidad_filas = t.shape[0]
+    cantidad_columnas = t.shape[1]
+    for i in range(1, cantidad_filas - 1):
+        for j in range(1, cantidad_columnas - 1):
+            if t[(i, j)] >= 4:
+                t = desbrodar_pocision(t, (i, j))
     if debug:
         print(t)
     return t
-# %% GENERAR TABLERO AL AZAR:
-def generar_tablero_azar(t, debug=False):
-    bordes(t)
-    animales_random(t)
+# %% 13. LUGARES A DESBORDAR devuelve True si hay alguna posición en el tablero que hay que desbordar, y False si no.
+def hay_que_desbordar(t, debug=False):
+    cantidad_filas = t.shape[0]
+    cantidad_columnas = t.shape[1]
+    falta = False
+    for i in range(1, cantidad_filas - 1):
+        for j in range(1, cantidad_columnas - 1):
+            if t[i, j] >= 4:
+                falta = True
+    if debug:
+        print(falta)
+    return falta
+# %% ESTABILIZAR mientras haya alguna posición que tenga al menos cuatro copos, llame a desbordar_valle.
+def estabilizar(t, debug=False):
+    while hay_que_desbordar(t):
+        desbordar_valle(t)
     if debug:
         print(t)
+    return t
+# %% COPO EN EL MEDIO:
+def paso(t, debug=False):
+    filas = t.shape[0]
+    columnas = t.shape[1]
+    punto_medio = ((int(filas / 2)), (int(columnas / 2)))
+    tirar_copo(t, (punto_medio))
+    estabilizar(t)
+    if debug:
+        print(t)
+    return t
+# %% VIDEO:
+def guardar_foto(t, paso):
+    dir_name = "output"
+    if not os.path.exists(dir_name):  # me fijo si no existe el directorio
+        os.mkdir(dir_name)  # si no existe lo creo
+    ax = plt.gca()
+    file_name = os.path.join(dir_name, "out{:05}.png".format(paso))
+    plt.imshow(t, vmin=-1, vmax=3)
+    ax.set_title("Copos tirados: {}".format(paso + 1))  # titulo
+    plt.savefig(file_name)
+def hacer_video(cant_fotos):
+    dir_name = "output"
+    lista_fotos = []
+    for i in range(cant_fotos):
+        file_name = os.path.join(dir_name, "out{:05}.png".format(i))
+        lista_fotos.append(imageio.imread(file_name))
+    video_name = os.path.join(dir_name, "avalancha.mp4")
+    # genero el video con 10 Copos por segundo. Explorar otros valores:
+    imageio.mimsave(video_name, lista_fotos, fps=10)
+    print("Estamos trabajando en el directorio", os.getcwd())
+    print("y se guardo el video:", video_name)
+def probar(n, pasos):
+    t = crear_tablero(n)
+    for i in range(pasos):
+        paso(t)
+        guardar_foto(t, i)
+    hacer_video(pasos)
+    return t
 # %% PARAMETROS:
-n = 10
-m = 10
-k = 10
-n_antilopes = 10
-n_leones = 3
+n = 7
+cant_fotos = 200
+pasos = 200
 
 # %% LLAMADAS:
-# t1 = crear_tablero(n, m)
-# bordes(t1)
-# # agregar_animales(t1, True)
-# animales_random(t1, True)
-# t2 = evolucionar(t1, True)
-# evolucionar(t2, True)
-# mezclar_celdas(t1, True)
-t1 = crear_tablero(n, m)
-generar_tablero_azar(t1, True)
-evolucionar_en_el_tiempo(t1, k, True)
+# t1 = tablero_base(7)
+# %% 
+probar(n, pasos)
